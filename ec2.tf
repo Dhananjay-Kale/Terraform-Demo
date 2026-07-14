3014bb0f6a1b5ceb845cb71374f410a68a37dda1
Dhananjay-Kale <dhananjaykaledk01@gmail.com>
2026-07-14 18:50:28 +0530

    Practicing terrafrom

---
+++ ec2.tf
@@ -0,0 +1,65 @@
+#Day-1
+#Provider-Block
+provider "aws" {
+    region = "ap-south-1"
+}
+
+
+#resource-block ---EC2
+#using count variable
+/*
+resource "aws_instance" "tf-server-1" {
+    ami = "ami-0b910d1016287a5e7"
+    instance_type = "t3.small"
+    #To crearte multiple instaces at SAME TIME
+    count = 3
+    #Using existing security group that have been created manually
+    vpc_security_group_ids = ["sg-06ec74401def8b075", "sg-0fa57c9479dcdc968"]
+    key_name = "xyz-key"
+    tags = {
+        Name = "server-${count.index}"
+    }  
+}
+
+
+#using for_each variable
+resource "aws_instance" "tf-server-1" {
+    ami = "ami-0b910d1016287a5e7"
+    instance_type = "t3.small"
+    #To crearte multiple instaces at SAME TIME
+    #count = 3
+    #for_each = toset[ "webserver", "appserver", "dbserver" ]
+    for_each = tomap({
+        app = "appserver"
+        web = "webserver"
+        db = "dbserver"
+            })
+    #Using existing security group that have been created manually
+    vpc_security_group_ids = ["sg-06ec74401def8b075", "sg-0fa57c9479dcdc968"]
+    key_name = "xyz-key"
+    tags = {
+        Name = "each"
+    }  
+}
+*/
+
+
+# task Day 1
+resource "aws_instance" "tf-server-1" {
+    ami = "ami-01a00762f46d584a1"
+    instance_type = "t3.micro"
+    #To crearte multiple instaces at SAME TIME
+    #count = 3
+    for_each = toset(["app-1", "db-1", "web-1"])
+    #for_each = tomap({
+    #    app = "appserver"
+    #   web = "webserver"
+    #   db = "dbserver"
+    #        })
+    #Using existing security group that have been created manually
+    vpc_security_group_ids = ["sg-06ec74401def8b075"]
+    key_name = "terraform-key"
+    tags = {
+        Name = "each"
+    }  
+}

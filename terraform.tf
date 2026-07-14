8b908768878fc917b0ce01357d4d73ba5cdf6fff
Dhananjay-Kale <dhananjaykaledk01@gmail.com>
2026-07-14 20:04:43 +0530

    Day 2

--- terraform.tf
+++ terraform.tf
@@ -66,4 +66,48 @@ resource "aws_instance" "tf-server-1" {
 */
 
 #Day 2
-Hello
+resource "aws_instance" "tf-server-2" {
+    ami = "ami-0b910d1016287a5e7"
+    instance_type = "t3.micro"
+    key_name = "devops-key"
+# Use security group created by terraform
+    vpc_security_group_ids = []
+    tags = {
+        Name = "server-2"
+    }
+}
+
+# resource -- to crearte security group
+resource "aws_security_group" "tf-SG" {
+    name = "my-sg"
+    vpc_id = "vpc-0ea0e58132290a92b"
+# Define inbound rule
+    ingress = {
+        description = "allow ssh"
+        to_port = 22
+        from_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        description = "alow http"
+        to_port = 80
+        from_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        description = "alow https"
+        to_port = 443
+        from_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        description = "alllow all traffic"
+        to_port = 0
+        from_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
